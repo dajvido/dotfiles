a7d8e7d6fea91089863c3b2df87e5d93d64c2535
Dawid Jaskot <dawid.jaskot@gmail.com>
2018-02-25 22:12:59 +0000

    Add i3 and dunst config

--- initial.sh
+++ initial.sh
@@ -2,3 +2,5 @@ ln -sfn dotfiles/ideavimrc $HOME/.ideavimrc
 ln -sfn dotfiles/gitconfig $HOME/.gitconfig
 ln -sfn dotfiles/vimrc $HOME/.vimrc
 ln -sfn dotfiles/vim $HOME/.vim
+ln -sfn dotfiles/i3 $HOME/.i3
+ln -sfn dotfiles/dunst $HOME/.config/dunst

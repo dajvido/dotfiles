46ce4103d32b0cbc894143def89ad62257f14067
Dawid Jaskot <dawid.jaskot@gmail.com>
2018-03-03 00:40:52 +0000

    Init alacritty configuration

--- initial.sh
+++ initial.sh
@@ -4,4 +4,5 @@ ln -sfn $HOME/dotfiles/vim $HOME/.vim
 ln -sfn $HOME/dotfiles/vim/init.vim $HOME/.vimrc
 ln -sfn $HOME/dotfiles/i3 $HOME/.i3
 ln -sfn $HOME/dotfiles/dunst $HOME/.config/dunst
+ln -sfn $HOME/dotfiles/alacritty $HOME/.config/alacritty
 ln -sfn $HOME/dotfiles/tmux.conf $HOME/.tmux.conf

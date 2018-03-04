19e1cdfde4960969d923074304b0c7a50367e2f8
Dawid Jaskot <dawid.jaskot@gmail.com>
2018-03-04 00:46:02 +0000

    Add some multimedia hotkeys

---
+++ i3/toggletouchpad.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+device='AlpsPS/2 ALPS DualPoint TouchPad'
+state=$(xinput list-props "$device" | grep "Device Enabled" | grep -o "[01]$")
+
+if [ $state == '1' ];then
+  xinput --disable "$device"
+else
+  xinput --enable "$device"
+fi

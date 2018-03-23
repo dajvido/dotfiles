d76721bfade1a0d666c4eff0565c0779b78414d4
Dawid Jaskot <dawid.jaskot@gmail.com>
2018-03-23 23:00:12 +0000

    Add kitty terminal config

--- initial.sh
+++ initial.sh
@@ -4,5 +4,6 @@ ln -sfn $HOME/dotfiles/vim $HOME/.vim
 ln -sfn $HOME/dotfiles/vim/init.vim $HOME/.vimrc
 ln -sfn $HOME/dotfiles/i3 $HOME/.i3
 ln -sfn $HOME/dotfiles/dunst $HOME/.config/dunst
-ln -sfn $HOME/dotfiles/alacritty $HOME/.config/alacritty
+ln -sfn $HOME/dotfiles/alacritty $HOME/.config
+ln -sfn $HOME/dotfiles/kitty $HOME/.config
 ln -sfn $HOME/dotfiles/tmux.conf $HOME/.tmux.conf

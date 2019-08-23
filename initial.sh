fe272e8aace0eb1df0f407a112ff0727cc0f68aa
Dawid Jaskot <dawid.jaskot@gmail.com>
2019-08-23 13:49:13 +0100

    updates

--- initial.sh
+++ initial.sh
@@ -7,6 +7,6 @@ ln -sfn $HOME/dotfiles/dunst $HOME/.config/dunst
 ln -sfn $HOME/dotfiles/alacritty $HOME/.config
 ln -sfn $HOME/dotfiles/kitty/custom.conf $HOME/.config/kitty
 ln -sfn $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
-ln -sfn $HOME/dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
-ln -sfn $HOME/dotfiles/vscode/snippets $HOME/Library/Application\ Support/Code/User
+ln -sfn $HOME/dotfiles/vscode/settings.json $HOME/Library/Application\ Support/Code\ -\ Insiders/User/settings.json
+ln -sfn $HOME/dotfiles/vscode/snippets $HOME/Library/Application\ Support/Code\ -\ Insiders/User
 ln -sfn $HOME/dotfiles/.hyper.js $HOME/.hyper.js
